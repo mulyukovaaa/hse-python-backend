fd501cdaeff2222b894c0f648b7a7891d519715e
mulyukovaaa <amina-mulukova.2002@yandex.ru>
2024-10-20 02:02:48 +0300

    hw 2

--- lecture_2/hw/shop_api/models.py
+++ lecture_2/hw/shop_api/models.py
@@ -1,19 +1,48 @@
-from pydantic import BaseModel # type: ignore
-from typing import List
+from typing import List, Optional
 
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+
+# То, что пользователь присылает для создания
+class WannaCreateItem(BaseModel):
+    name: str = Field(..., min_length=1, description="Name must be a non-empty string")
+    price: float = Field(..., ge=0, description="Price must be a non-negative value")
+
+
+# Модель для хранения товара
 class Item(BaseModel):
-    id: int
+    id: UUID
     name: str
     price: float
     deleted: bool = False
 
-class CartItem(BaseModel):
-    id: int
+
+# Модель корзины с товарами
+class Cart(BaseModel):
+    id: UUID
+    items: List[UUID] = Field(default_factory=list)
+
+
+# Товар, который лежит в корзине
+class CartInItem(BaseModel):
+    id: UUID
     name: str
     quantity: int
     available: bool
 
-class Cart(BaseModel):
-    id: int
-    items: List[CartItem]
+
+# Модель корзины для ответа о корзине
+class CartAnswerList(BaseModel):
+    id: UUID
+    items: List[CartInItem]
     price: float
+
+
+# Класс на обновление
+class UpdateItem(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, description="Name must be a non-empty string")
+    price: Optional[float] = Field(None, ge=0, description="Price must be a non-negative value")
+
+    class Config:
+        extra = "forbid"
